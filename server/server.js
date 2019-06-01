5daa839d0912dafd4eb51463998c9050575db7d2
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2019-06-01 12:09:02 +0530

    Added Get /todos route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 
@@ -20,6 +20,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () =>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+})
+
+app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
+
+module.exports = { app };
